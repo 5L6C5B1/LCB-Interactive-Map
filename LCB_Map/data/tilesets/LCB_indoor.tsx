53d03d238c7dc25cea60a025c774e3fbff623d8b
rievelluci <147370986+5L6C5B1@users.noreply.github.com>
2025-07-17 00:50:49 +0800

    Add tilesets

---
+++ LCB_Map/data/tilesets/LCB_indoor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="LCB_indoor" tilewidth="64" tileheight="64" tilecount="700" columns="25">
+ <image source="../../../LCB_Map/graphics/tilesets/indoor.png" width="1600" height="1792"/>
+</tileset>
